4c0bc1c391171b857f76f29415b6d9e68c65f4b2
Khriztian9 <85960917+Khriztian9@users.noreply.github.com>
2025-07-02 11:40:02 -0500

    Use react-fullpage for section scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,50 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Hero2 from '../components/Hero2';
+import MapaZelvamar from '../components/MapaZelvamar';
 import Rooms from '../components/Rooms';
+import Precios from '../components/Precios';
 import CarouselCard from '../components/CarouselCard';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
-import Hero2 from '../components/Hero2';
-import MapaZelvamar from '../components/MapaZelvamar';
-import Precios from '../components/Precios';
-
-
-
-
+import ReactFullpage from '@fullpage/react-fullpage';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Hero2 />
-      <MapaZelvamar />
-
-
-
-   <div style={{ overflow: 'hidden', lineHeight: 0 }}>
-      <svg viewBox="0 0 1440 320" style={{ width: '100%', height: '100px' }} preserveAspectRatio="none">
-        <path
-          fill="#000"
-          d="M0,160 C480,260 960,60 1440,160 L1440,320 L0,320 Z"
-        />
-      </svg>
-    </div>
-
-      <Rooms />
-
-      <div style={{ overflow: 'hidden', lineHeight: 0 }}>
-      <svg viewBox="0 0 1440 320" style={{ width: '100%', height: '100px' }} preserveAspectRatio="none">
-        <path
-          fill="#000"
-          d="M0,160 C480,260 960,60 1440,160 L1440,320 L0,320 Z"
-        />
-      </svg>
-    </div>
-
-
-      <Precios />
-
-
-    <div style={{ overflow: 'hidden', lineHeight: 0 }}>
-      <svg viewBox="0 0 1440 320" style={{ width: '100%', height: '100px' }} preserveAspectRatio="none">
-        <path
-          fill="#000"
-          d="M0,160 C480,260 960,60 1440,160 L1440,320 L0,320 Z"
-        />
-      </svg>
-    </div>
-
-      <CarouselCard />
-
-       <div style={{ overflow: 'hidden', lineHeight: 0 }}>
-      <svg viewBox="0 0 1440 320" style={{ width: '100%', height: '100px' }} preserveAspectRatio="none">
-        <path
-          fill="#000"
-          d="M0,160 C480,260 960,60 1440,160 L1440,320 L0,320 Z"
-        />
-      </svg>
-    </div>
-
-      <Contact />
-      <Footer />
+      <ReactFullpage
+        navigation
+        scrollingSpeed={700}
+        render={() => (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <Hero />
+            </div>
+            <div className="section">
+              <Hero2 />
+            </div>
+            <div className="section">
+              <MapaZelvamar />
+            </div>
+            <div className="section">
+              <Rooms />
+            </div>
+            <div className="section">
+              <Precios />
+            </div>
+            <div className="section">
+              <CarouselCard />
+            </div>
+            <div className="section">
+              <Contact />
+            </div>
+            <div className="section">
+              <Footer />
+            </div>
+          </ReactFullpage.Wrapper>
+        )}
+      />
     </>
   );
 }
