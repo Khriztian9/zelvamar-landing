8e8dba16b10bac343dbf7d24871909b239a4b893
Khriztian9 <85960917+Khriztian9@users.noreply.github.com>
2025-07-02 13:01:52 -0500

    Revert fullpage scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,44 +7,19 @@ import Precios from '../components/Precios';
 import CarouselCard from '../components/CarouselCard';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
-import ReactFullpage from '@fullpage/react-fullpage';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <ReactFullpage
-        navigation
-        scrollingSpeed={700}
-        render={() => (
-          <ReactFullpage.Wrapper>
-            <div className="section">
-              <Hero />
-            </div>
-            <div className="section">
-              <Hero2 />
-            </div>
-            <div className="section">
-              <MapaZelvamar />
-            </div>
-            <div className="section">
-              <Rooms />
-            </div>
-            <div className="section">
-              <Precios />
-            </div>
-            <div className="section">
-              <CarouselCard />
-            </div>
-            <div className="section">
-              <Contact />
-            </div>
-            <div className="section">
-              <Footer />
-            </div>
-          </ReactFullpage.Wrapper>
-        )}
-      />
+      <Hero />
+      <Hero2 />
+      <MapaZelvamar />
+      <Rooms />
+      <Precios />
+      <CarouselCard />
+      <Contact />
+      <Footer />
     </>
   );
 }
